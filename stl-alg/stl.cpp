fd17c2812a11e19e4fdc4d4bb693d1aafe4112d7
Marek Wilkosz <mrwilkosz@gmail.com>
2014-08-22 20:23:58 +0200

    stl basics

--- stl-alg/stl.cpp
+++ stl-alg/stl.cpp
@@ -4,12 +4,26 @@
 #include <algorithm>
 #include <atomic>
 
+constexpr uint32_t ver = 3.0F;
+static_assert(ver >= 3U, "Version is too low");
+
 namespace
 {
-    //undefined reference to __atomic_load_4
+    //avr stdlibc++ lacks __atomic_load_4
     //volatile std::atomic<std::uint32_t> system_tick;
-    
     volatile std::atomic<std::uint8_t> system_tick;
+
+    typedef std::uint32_t cnt_t;
+    std::array<cnt_t, 4U> counters;
+    
+    std::array<int, 3U> u
+    {
+        {1, 2, 3}
+    };
+    std::array<int, 3U> v
+    {
+        {2, 3, 4}
+    };
 }
 
 std::uint32_t get_system_time()
@@ -17,9 +31,26 @@ std::uint32_t get_system_time()
     return std::atomic_load(&system_tick);
 }
 
+void fill_counters()
+{
+    std::fill(counters.begin(),
+              counters.end(),
+              std::numeric_limits<cnt_t>::max());
+}
+
+int get_inner_product()
+{
+    return
+    std::inner_product(u.begin(), 
+                       u.end(),
+                       v.begin(),
+                       0); //?
+}
+
 int main()
 {
    get_system_time(); 
-   return 0;
+   fill_counters();
+   return get_inner_product();
 }
 
